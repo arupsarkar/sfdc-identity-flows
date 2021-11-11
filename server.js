e7debbfe74450e732995e767e4959cffa894f913
arupsarkar <sarkar.arup@gmail.com>
2021-11-11 00:36:11 -0500

    Added get accounts query

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res, next) => {
     res.json("hello world !!")
 })
 // to test the URL
-// http://localhost:8081/login?username=arup&password=foo&loginUrl=https://login.salesforce.com
+// Testing: http://localhost:8081/login?username=arup.dotnet@sfdc.com&password=Salesforce4C01Yp5elgy1bBjP3QmRFfFGA&loginUrl=https://login.salesforce.com
 app.get('/login', async (req, res, next) => {
     let username = req.query.username
     let password = req.query.password
@@ -26,17 +26,35 @@ app.get('/login', async (req, res, next) => {
     const { conn, userInfo } = await init(username, password, loginUrl)
     console.log(conn)
     if(conn.accessToken) {
-        res.json({"access token": conn.accessToken})
+        res.json({"instanceUrl": conn.instanceUrl, "accessToken": conn.accessToken})
     }else {
         res.json({"Error": "Error logging in"})
     }
 
 })
 
+// get accounts
+// Testing: http://localhost:8081/accounts?instanceUrl=https://azure-integration-dev-ed.my.salesforce.com&accessToken=00Df4000004kCY8!AQsAQP1mKIYvqQBMQFNYNrWDzgYemPHwbNxGJyHaDkQPFwYvlJI_byti0YKU_WPGzL5RUXJsNjvdVxboBxQhkJsIJRw90Kw1&searchParam=1234
+app.get('/accounts', async (req, res, next) =>{
+    let instanceUrl = decodeURIComponent(encodeURIComponent(req.query.instanceUrl))
+    let accessToken = decodeURIComponent(encodeURIComponent(req.query.accessToken))
+    let searchParam = req.query.searchParam
+
+    //create the conn object
+    let conn = new jsforce.Connection({
+        instanceUrl : instanceUrl,
+        accessToken: accessToken
+    });
+
+    let accounts = await conn.search(`FIND {${searchParam}} IN ALL FIELDS RETURNING Account`)
+    res.json(accounts)
+
+})
+
 app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'))
 })
-// /login?username=arup.dotnet@sfdc.com&password=Salesforce4C01Yp5elgy1bBjP3QmRFfFGA&loginUrl=https://login.salesforce.com
+//
 function init(username, password, loginUrl) {
     console.log('username', username)
     console.log('password', password)
