b4bd345fc818d71aee7d73795698a3888010463a
arupsarkar <sarkar.arup@gmail.com>
2021-11-30 16:30:30 -0500

    Added username and password form

--- client/src/components/Login/UsernamePasswordForm/UsernamePasswordForm.js
+++ client/src/components/Login/UsernamePasswordForm/UsernamePasswordForm.js
@@ -1,6 +1,7 @@
-import React, {} from 'react';
+import React, {useState} from 'react';
 
 import { useSelector } from "react-redux";
+import {use} from "express/lib/router";
 
 const UsernamePasswordForm = () => {
 
@@ -10,12 +11,66 @@ const UsernamePasswordForm = () => {
     //     console.log(protocol.protocol.value.id)
     //     console.log(protocol.protocol.value.protocol)
     // }
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
+    const handleUsernameChange = (event) => {
+
+        setUsername(event.target.value)
+        //console.log(username)
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+        //console.log(password)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(username, password)
+        const headers = {'Content-Type': 'application/json'}
+        const url = `https://sfdc-identity-flows.herokuapp.com/api/login?username=${username}&password=${password}&loginUrl=https://login.salesforce.com`
+
+        fetch(url, {headers})
+            .then(async response => {
+                await response.json()
+            })
+            .then(data => {
+                console.log(data.accessToken)
+                console.log(data.instanceUrl)
+            })
+            .catch(error => {
+                console.error('There was an error : ', error)
+            })
+
+    }
     return(
-        <div>
-            <h2>Username : </h2>
-            <h2>Password+Token : </h2>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    Username :
+                    <input type="text" name="username"
+                           onChange={handleUsernameChange}/>
+                </label>
+                <div
+                    style={{
+                        paddingTop: '20px',
+                        boxSizing: 'content-box'
+
+                    }}>
+
+                    <label>
+                        Password+Token :
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={handlePasswordChange}/>
+                    </label>
+
+                </div>
+                <button> Login </button>
+
+            </div>
+        </form>
+
     )
 }
 
