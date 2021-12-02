2f199ecd1ca6035b7dcafb8ed9aab246d7333e43
arupsarkar <sarkar.arup@gmail.com>
2021-12-01 21:52:47 -0500

    Added fetch to data from server

--- client/src/components/Accounts/Accounts.js
+++ client/src/components/Accounts/Accounts.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import styles from './Accounts.module.css';
-import {selectTokens} from "../Login/identityTokensSlice";
+import {saveTokens, selectTokens} from "../Login/identityTokensSlice";
 import {useSelector} from "react-redux";
 import {Button, Grid} from "@mui/material";
 
@@ -10,6 +10,7 @@ const Accounts = () => {
     const [accessToken, setAccessToken] = useState('dummyToken')
     const [instanceUrl, setInstanceUrl] = useState('dummyUrl')
     const [loggedIn, setLoggedIn] = useState(false)
+    const[accounts, setAccounts] = useState('no data')
 
     // try{
     //     if(tokens.value.accessToken && tokens.value.instanceUrl){
@@ -21,12 +22,29 @@ const Accounts = () => {
     //     console.error(error)
     // }
 
-    const getAccounts = (accessToken, instanceUrl) => {
-        setAccessToken(accessToken)
-        setInstanceUrl(instanceUrl)
+    const getAccounts = (access_token, instanceURL) => {
+        setAccessToken(access_token)
+        setInstanceUrl(instanceURL)
         setLoggedIn(true)
         console.log(accessToken)
         console.log(instanceUrl)
+        const headers = {'Content-Type': 'application/json'}
+        const url = `https://sfdc-identity-flows.herokuapp.com/api/accounts/find?instanceUrl=${instanceUrl}&accessToken=${accessToken}&searchParam=123456`
+
+        fetch(url, {headers})
+            .then(async response => {
+                const data = await response.json()
+
+                if(!response.ok) {
+                    const error = (data && data.message) || response.statusText + ', code: ' + response.statusCode
+                    return Promise.reject(error)
+                }
+                setAccounts(data)
+                console.log({accounts})
+            })
+            .catch(error => {
+                console.error('There was an error : ', error)
+            })
     }
 
     return (
@@ -46,8 +64,14 @@ const Accounts = () => {
             </div>
             <Grid container justifyContent={"center"}>
                 <div className={styles.Accounts} data-testid="Accounts">
-                    {tokens.value.accessToken}
-                    {tokens.value.instanceUrl}
+                    <div>
+                        {tokens.value.accessToken}
+                    </div>
+                    <div>
+                        {tokens.value.instanceUrl}
+                    </div>
+
+
                 </div>
             </Grid>
 
