953dbcf7850d03ef713ad8d922d2e8d6160e0100
arupsarkar <sarkar.arup@gmail.com>
2021-12-22 22:38:07 -0500

    Added pre flight cors

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 8081
 //const app = express()
 const app = require('./app')
 // enable all cors request
-app.options('*', cors())
+
 app.use(cors())
+app.options('*', cors())
+// Initialize session
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
