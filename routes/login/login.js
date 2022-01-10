e0e30db93c08584b9900a0348be0aff0c17931db
arupsarkar <sarkar.arup@gmail.com>
2022-01-10 15:46:07 -0500

    Added res redirect with payload

--- routes/login/login.js
+++ routes/login/login.js
@@ -76,10 +76,14 @@ router.get('/oauth2-token/callback', async (req, res, next) => {
         console.log('---> refresh token ', conn.refreshToken)
         console.log('---> req session values ', `${req.session.accessToken} ${req.session.instanceUrl}`)
         console.log('---> OAuth2 result', result)
-        res.redirect('/app')
+        res.redirect('/app/accounts')
         // res.json({'result': result})
     })
 
+})
+
+router.get('/app/accounts', urlencodedParser, async (req, res, next) => {
+    res.send(req.session.accessToken + '/n' + req.session.instanceUrl)
 })
 
 router.get('/', urlencodedParser, async (req, res, next) => {
