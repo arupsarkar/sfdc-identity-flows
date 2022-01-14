878d90f331b97167aa405640a0783558029b6124
arupsarkar <sarkar.arup@gmail.com>
2022-01-13 20:41:52 -0500

    Added WebSocket layer, modified server to incorporate ws

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const port = process.env.PORT || 8081
 const app = require('./app')
-app.listen(port, () => {
+
+const webSocketServer = require('websocket').server
+const http = require('http')
+const server = http.createServer(app)
+server.listen(port, () => {
     console.log(`Server listening on ${port}`)
 })
 
